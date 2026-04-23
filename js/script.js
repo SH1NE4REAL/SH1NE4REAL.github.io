36e1d50d4aeaf49319fa79fba525bee46102a5c7
shenghao <3138326159@QQ.com>
2026-04-23 16:23:07 +0800

    Site updated: 2026-04-23 16:23:06

--- js/script.js
+++ js/script.js
@@ -135,4 +135,26 @@
 
     $container.removeClass('mobile-nav-on');
   });
+  // Reveal animation
+  var revealEls = document.querySelectorAll('.reveal');
+
+  if ('IntersectionObserver' in window && revealEls.length) {
+    var revealObserver = new IntersectionObserver(function(entries){
+      entries.forEach(function(entry){
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-visible');
+        }
+      });
+    }, {
+      threshold: 0.15
+    });
+
+    revealEls.forEach(function(el){
+      revealObserver.observe(el);
+    });
+  } else {
+    revealEls.forEach(function(el){
+      el.classList.add('is-visible');
+    });
+  }
 })(jQuery);
